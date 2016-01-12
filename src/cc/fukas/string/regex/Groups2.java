d021399fd9b2271556f4c6385c91adcd961b91ef
dendy <sunfuchang03@126.com>
2016-01-12 22:49:20 +0800

    add regex code

---
+++ src/cc/fukas/string/regex/Groups2.java
@@ -0,0 +1,69 @@
+package cc.fukas.string.regex;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static cc.fukas.util.PrintHelper.*;
+
+/**
+ * Created by sun on 16-1-12.
+ *
+ * @author sun
+ * @version 1.0
+ * @since 1.0
+ */
+public class Groups2 {
+    //~ Static fields/initializers =====================================================================================
+
+    //~ Instance fields ================================================================================================
+
+    //~ Methods ========================================================================================================
+
+    /**
+     * 查询不以大写字母开头的单词，并统计每词的数量
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        String words = Groups.POEM;
+        // 不以大写字母开头的单词
+        String regex = "\\b([a-z]\\w*)\\b";
+        Matcher matcher = Pattern.compile(regex).matcher(words);
+        Map<String, Integer> map = new HashMap<>();
+        while (matcher.find()) {
+            String s = matcher.group(1);
+            Integer cnt = map.get(s);
+            if (cnt == null)
+                cnt = 0;
+            map.put(s, ++cnt);
+            print("[" + s + "]");
+        }
+        println();
+        for (String s : map.keySet()) {
+            println(s + " : " + map.get(s));
+        }
+    }
+}
+/* Output :
+[quietly][take][my][leave][quietly][as][came][here][wave][good][bye][the][rosy][clouds][in][the][western][sky]
+here : 1
+sky : 1
+in : 1
+clouds : 1
+my : 1
+good : 1
+wave : 1
+bye : 1
+the : 2
+take : 1
+as : 1
+leave : 1
+rosy : 1
+came : 1
+western : 1
+quietly : 2
+ */
---
+++ src/cc/fukas/string/regex/PatternUsing2.java
@@ -0,0 +1,32 @@
+package cc.fukas.string.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static cc.fukas.util.PrintHelper.println;
+
+/**
+ * Created by sun on 16-1-12.
+ *
+ * @author sun
+ * @version 1.0
+ * @since 1.0
+ */
+public class PatternUsing2 {
+    //~ Static fields/initializers =====================================================================================
+
+    //~ Instance fields ================================================================================================
+
+    static String words = "Arline ate eight apples and one orange while Anita hadn't any";
+
+    //~ Methods ========================================================================================================
+
+    public static void main(String[] args) {
+        // 匹配一元音字母或空格+原因字母开头，并且以原因字母结束的单词
+        String regex = "(?i)((^[aeiou])|(\\s+[aeiou]))\\w+?[aeiou]\\b";
+        Matcher matcher = Pattern.compile(regex).matcher(words);
+        while (matcher.find()) {
+            println(matcher.group());
+        }
+    }
+}
