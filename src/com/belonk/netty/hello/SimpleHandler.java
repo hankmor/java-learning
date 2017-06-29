0fdbc64e6486b9a6f8a424aff93317daf73e9c26
sun <sunfuchang03@126.com>
2017-06-29 21:20:18 +0800

    thread

---
+++ src/com/belonk/netty/hello/SimpleHandler.java
@@ -0,0 +1,42 @@
+package com.belonk.netty.hello;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Created by sun on 2017/4/16.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class SimpleHandler extends ChannelInboundHandlerAdapter {
+    //~ Static fields/initializers =====================================================================================
+
+
+    //~ Instance fields ================================================================================================
+
+
+    //~ Constructors ===================================================================================================
+
+
+    //~ Methods ========================================================================================================
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        while (in.isReadable()) {
+            System.out.print((char) in.readByte());
+            System.out.flush();
+        }
+        ReferenceCountUtil.release(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
