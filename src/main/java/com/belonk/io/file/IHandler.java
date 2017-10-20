5f3f94b0b840b1241e7d4d39e93bb0dc826a455b
sun <sunfuchang03@126.com>
2017-10-21 00:22:00 +0800

    change to maven project

---
+++ src/main/java/com/belonk/io/file/IHandler.java
@@ -0,0 +1,12 @@
+package main.java.com.belonk.io.file;
+
+/**
+ * Created by sun on 2017/9/20.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public interface IHandler {
+    void handle(String line);
+}
---
+++ src/main/java/com/belonk/jdk8/interfaces/InterfaceTest.java
@@ -0,0 +1,34 @@
+package main.java.com.belonk.jdk8.interfaces;
+
+/**
+ * Created by sun on 2017/6/29.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class InterfaceTest {
+    //~ Static fields/initializers =====================================================================================
+
+
+    //~ Instance fields ================================================================================================
+
+
+    //~ Constructors ===================================================================================================
+
+
+    //~ Methods ========================================================================================================
+    public static void main(String[] args) {
+        Calculator calculator = new SimpleCalculator();
+        double d = calculator.calc(10);
+        System.out.println(d);
+        calculator = new Calculator() {
+            @Override
+            public double calc(int n) {
+                return sqrt(n);
+            }
+        };
+        System.out.println(calculator.calc(9));
+        System.out.println(calculator.sqrt(9));
+    }
+}
