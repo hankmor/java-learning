760708d408a42aea0a4df33805733df269b74be6
huzhou <huzhou.v@gmail.com>
2018-06-23 10:48:34 +0800

    add future test code

---
+++ src/main/java/com/belonk/concurrent/basic/ThreadVariations.java
@@ -0,0 +1,197 @@
+package com.belonk.concurrent.basic;
+
+import com.belonk.util.PrintHelper;
+
+// 内部类实现线程
+class InnerThread1 {
+    private int countDown = 5;
+    private Inner inner;
+
+    public InnerThread1(String name) {
+        inner = new Inner(name);
+    }
+
+    private class Inner extends Thread {
+        Inner(String name) {
+            super(name);
+            start(); // 启动线程
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    PrintHelper.println(this);
+                    if (--countDown == 0) return;
+                    sleep(10);
+                }
+            } catch (InterruptedException e) {
+                PrintHelper.println("interrupted");
+            }
+        }
+
+        @Override
+        public String toString() {
+            return getName() + "：" + countDown;
+        }
+    }
+}
+
+// 匿名内部类创建线程
+class InnerThread2 {
+    private int countDown = 5;
+
+    public InnerThread2(String name) {
+        // 创建匿名线程
+        new Thread(name) {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        while (true) {
+                            PrintHelper.println(this);
+                            if (--countDown == 0) return;
+                            sleep(10);
+                        }
+                    } catch (InterruptedException e) {
+                        PrintHelper.println("interrupted");
+                    }
+                }
+            }
+
+            @Override
+            public String toString() {
+                return getName() + "：" + countDown;
+            }
+        }.start();
+    }
+}
+
+class InnerRunnable1 {
+    private int countDown = 5;
+    private Inner inner;
+
+    public InnerRunnable1(String name) {
+        inner = new Inner(name);
+    }
+
+    private class Inner implements Runnable {
+        Thread t;
+
+        Inner(String name) {
+            t = new Thread(this, name);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    PrintHelper.println(this);
+                    if (--countDown == 0) return;
+                    Thread.sleep(10);
+                }
+            } catch (InterruptedException e) {
+                PrintHelper.println("interrupted");
+            }
+        }
+
+        @Override
+        public String toString() {
+            return t.getName() + "：" + countDown;
+        }
+    }
+}
+
+class InnerRunnable2 {
+    private int countDown = 5;
+
+    public InnerRunnable2(String name) {
+        // 创建匿名线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        while (true) {
+                            PrintHelper.println(this);
+                            if (--countDown == 0) return;
+                            Thread.sleep(10);
+                        }
+                    } catch (InterruptedException e) {
+                        PrintHelper.println("interrupted");
+                    }
+                }
+            }
+
+            @Override
+            public String toString() {
+                return Thread.currentThread().getName() + "：" + countDown;
+            }
+        }, name).start();
+    }
+}
+
+class ThreadMethod {
+    private int countDown = 5;
+    private Thread t;
+    private String name;
+
+    public ThreadMethod(String name) {
+        this.name = name;
+    }
+
+    public void runTask() {
+        if (t == null) {
+            t = new Thread(name) {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            while (true) {
+                                PrintHelper.println(this);
+                                if (--countDown == 0) return;
+                                sleep(10);
+                            }
+                        } catch (InterruptedException e) {
+                            PrintHelper.println("interrupted");
+                        }
+                    }
+                }
+
+                @Override
+                public String toString() {
+                    return Thread.currentThread().getName() + "：" + countDown;
+                }
+            };
+        }
+        t.start();
+    }
+}
+
+/**
+ * Created by sun on 2017/3/6.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class ThreadVariations {
+    //~ Static fields/initializers =====================================================================================
+
+
+    //~ Instance fields ================================================================================================
+
+
+    //~ Constructors ===================================================================================================
+
+
+    //~ Methods ========================================================================================================
+    public static void main(String[] args) {
+        new InnerThread1("InnerThread1");
+        new InnerThread2("InnerThread2");
+        new InnerRunnable1("InnerRunnable1");
+        new InnerRunnable2("InnerRunnable2");
+        new ThreadMethod("ThreadMethod").runTask();
+    }
+}
---
+++ src/main/java/com/belonk/concurrent/executor/SingleThreadExecutor.java
@@ -0,0 +1,42 @@
+package com.belonk.concurrent.executor;
+
+import com.belonk.concurrent.basic.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by sun on 2017/3/5.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class SingleThreadExecutor {
+    //~ Static fields/initializers =====================================================================================
+
+
+    //~ Instance fields ================================================================================================
+
+
+    //~ Constructors ===================================================================================================
+
+
+    //~ Methods ========================================================================================================
+
+    public static void main(String[] args) {
+        // 多个任务排队顺序执行
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new LiftOff());
+        }
+        executorService.shutdown();
+    }
+}
+/**
+ * #0(9), #0(8), #0(7), #0(6), #0(5), #0(4), #0(3), #0(2), #0(1), #0(LiftOff!),
+ * #1(9), #1(8), #1(7), #1(6), #1(5), #1(4), #1(3), #1(2), #1(1), #1(LiftOff!),
+ * #2(9), #2(8), #2(7), #2(6), #2(5), #2(4), #2(3), #2(2), #2(1), #2(LiftOff!),
+ * #3(9), #3(8), #3(7), #3(6), #3(5), #3(4), #3(3), #3(2), #3(1), #3(LiftOff!),
+ * #4(9), #4(8), #4(7), #4(6), #4(5), #4(4), #4(3), #4(2), #4(1), #4(LiftOff!),
+ */
