6e7215b67941ffd8f9aeff3bb7a49ec731d3508e
sam <belonk@126.com>
2021-04-11 22:39:52 +0800

    thread state demo

--- src/main/java/com/belonk/image/WebpImageTest.java
+++ src/main/java/com/belonk/image/WebpImageTest.java
@@ -1,8 +1,5 @@
 package com.belonk.image;
 
-import javax.imageio.ImageIO;
-import javax.media.jai.JAI;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,41 +15,41 @@ import java.io.IOException;
  * @since 1.0
  */
 public class WebpImageTest {
-    //~ Static fields/initializers =====================================================================================
+	//~ Static fields/initializers =====================================================================================
 
 
-    //~ Instance fields ================================================================================================
+	//~ Instance fields ================================================================================================
 
 
-    //~ Constructors ===================================================================================================
+	//~ Constructors ===================================================================================================
 
 
-    //~ Methods =======================================================================================================
+	//~ Methods =======================================================================================================
 
 
-    public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\Administrator\\Desktop\\640.jpg";
+	public static void main(String[] args) throws IOException {
+		String path = "C:\\Users\\Administrator\\Desktop\\640.jpg";
 //        BufferedImage bi = ImageIO.read(new File(path));
 //        System.out.println(bi);
-        String javaLibPath = System.getProperty("(java.library.path");
-        System.out.println(javaLibPath);
+		String javaLibPath = System.getProperty("(java.library.path");
+		System.out.println(javaLibPath);
 
-        File file = new File(path);
+		File file = new File(path);
 
-        try {
-            FileInputStream in = new FileInputStream(file);
-            byte[] bytes = new byte[30];
-            in.read(bytes);
-            if ("WEBP".equals(new String(bytes, 8, 4, "utf-8"))) {//先判断图片格式为webp
-                int w = (((int) bytes[27] & 0xff) << 8) | (int) bytes[26] & 0xff;
-                int h = (((int) bytes[29] & 0xff) << 8) | (int) bytes[28] & 0xff;
-                System.out.print("宽:" + w + ",高:" + h);
-            } else {
-                System.out.print("文件不是webp格式");
-            }
-            in.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
+		try {
+			FileInputStream in = new FileInputStream(file);
+			byte[] bytes = new byte[30];
+			in.read(bytes);
+			if ("WEBP".equals(new String(bytes, 8, 4, "utf-8"))) {//先判断图片格式为webp
+				int w = (((int) bytes[27] & 0xff) << 8) | (int) bytes[26] & 0xff;
+				int h = (((int) bytes[29] & 0xff) << 8) | (int) bytes[28] & 0xff;
+				System.out.print("宽:" + w + ",高:" + h);
+			} else {
+				System.out.print("文件不是webp格式");
+			}
+			in.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
