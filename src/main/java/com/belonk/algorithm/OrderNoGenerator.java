bce6004e07ba4e668966a13cad8824377ae2c11c
sam <belonk@126.com>
2021-04-14 16:43:06 +0800

    update

--- src/main/java/com/belonk/algorithm/OrderNoGenerator.java
+++ src/main/java/com/belonk/algorithm/OrderNoGenerator.java
@@ -10,35 +10,37 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @since 1.0
  */
 public class OrderNoGenerator {
-    //~ Static fields/initializers =====================================================================================
+	//~ Static fields/initializers =====================================================================================
 
 
-    //~ Instance fields ================================================================================================
-    private final int maxValue;
-    private final AtomicInteger atomicInteger;
+	//~ Instance fields ================================================================================================
+	private final int maxValue;
+	private final AtomicInteger atomicInteger;
 
-    //~ Constructors ===================================================================================================
+	//~ Constructors ===================================================================================================
 
 
-    //~ Methods ========================================================================================================
+	//~ Methods ========================================================================================================
 
-    public OrderNoGenerator(final int minValue, final int maxValue) {
-        this.atomicInteger = new AtomicInteger(minValue);
-        this.maxValue = maxValue;
-    }
+	public OrderNoGenerator(final int minValue, final int maxValue) {
+		this.atomicInteger = new AtomicInteger(minValue);
+		this.maxValue = maxValue;
+	}
 
-    public Integer incrAndGet() {
-        final int value = this.atomicInteger.incrementAndGet();
-        if (value > maxValue) {
-            return null;
-        }
-        return value;
-    }
+	public Integer incrAndGet() {
+		final int value = this.atomicInteger.incrementAndGet();
+		if (value > maxValue) {
+			return null;
+		}
+		return value;
+	}
 
-    public static void main(String[] args) {
-        OrderNoGenerator atomicRangeInteger = new OrderNoGenerator(0, 1 << 32 - 1);
-        final long currentTimeMillis = System.currentTimeMillis();
-        Long id = currentTimeMillis << 32 | atomicRangeInteger.incrAndGet();
-        System.out.println(id);
-    }
+	public static void main(String[] args) {
+		OrderNoGenerator atomicRangeInteger = new OrderNoGenerator(0, /*(1 << 31) - 1*/Integer.MAX_VALUE);
+		final long currentTimeMillis = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			Long id = currentTimeMillis << 31 | atomicRangeInteger.incrAndGet();
+			System.out.println(id);
+		}
+	}
 }
