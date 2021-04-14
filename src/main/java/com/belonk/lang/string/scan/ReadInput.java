1fe10009f6ff349a96268d9c4610796777109261
sam <belonk@126.com>
2021-04-14 16:30:39 +0800

    refactor packages.

---
+++ src/main/java/com/belonk/lang/string/scan/ReadInput.java
@@ -0,0 +1,29 @@
+package com.belonk.lang.string.scan;
+
+import java.util.Scanner;
+
+import static com.belonk.util.PrintHelper.println;
+
+/**
+ * <p>Created by sun on 2016/1/13.
+ *
+ * @author sun
+ * @version 1.0
+ * @since 2.2.3
+ */
+public class ReadInput {
+    //~ Static fields/initializers =====================================================================================
+
+    //~ Instance fields ================================================================================================
+
+    //~ Methods ========================================================================================================
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("sun\n29 black");
+        println("What's you name?");
+        println(scanner.nextLine());
+        println("How old are you?");
+        println(scanner.nextInt());
+        println("What's you favorite color?");
+        println(scanner.next());
+    }
+}
