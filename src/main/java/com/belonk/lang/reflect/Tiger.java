76a0b014d4193fc438bda7f6301a588ff9c045c7
sam <koobyte@126.com>
2021-11-08 00:17:32 +0800

    添加动态代理实现混型demo

---
+++ src/main/java/com/belonk/lang/reflect/Tiger.java
@@ -0,0 +1,25 @@
+package com.belonk.lang.reflect;
+
+/**
+ * Created by sun on 2021/11/7.
+ *
+ * @author sunfuchang03@126.com
+ * @since 3.0
+ */
+public class Tiger implements Run {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	@Override
+	public void run(int meter) {
+		System.out.println("tiger ran " + meter + " meters.");
+	}
+}
