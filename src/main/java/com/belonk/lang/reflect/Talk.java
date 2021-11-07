26e8d20ad42e5db5f3bbc8b706a8f456aacb7b28
huzhou <huzhou.v@gmail.com>
2021-11-08 00:17:32 +0800

    添加动态代理实现混型demo

---
+++ src/main/java/com/belonk/lang/reflect/Talk.java
@@ -0,0 +1,16 @@
+package com.belonk.lang.reflect;
+
+/**
+ * Created by sun on 2021/11/7.
+ *
+ * @author sunfuchang03@126.com
+ * @since 3.0
+ */
+public interface Talk {
+	//~ Constants/Initializer
+
+
+	//~ Interfaces
+
+	void talk(String content);
+}
---
+++ src/main/java/com/belonk/lang/reflect/Think.java
@@ -0,0 +1,16 @@
+package com.belonk.lang.reflect;
+
+/**
+ * Created by sun on 2021/11/7.
+ *
+ * @author sunfuchang03@126.com
+ * @since 3.0
+ */
+public interface Think {
+	//~ Constants/Initializer
+
+
+	//~ Interfaces
+
+	void think(String content);
+}
