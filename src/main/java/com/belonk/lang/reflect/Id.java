ae19d6ca746bd1d79c263327bd463f9b06bca1a1
huzhou <huzhou.v@gmail.com>
2021-11-09 01:00:25 +0800

    reflect demo

---
+++ src/main/java/com/belonk/lang/reflect/Id.java
@@ -0,0 +1,29 @@
+package com.belonk.lang.reflect;
+
+/**
+ * Created by sun on 2021/11/8.
+ *
+ * @author sunfuchang03@126.com
+ * @since 3.0
+ */
+public class Id {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+	private int id;
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
