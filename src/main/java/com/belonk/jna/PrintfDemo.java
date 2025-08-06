ae2fe319c084657f28ee8d366defe55b7e662136
hank <hankmo.x@gmail.com>
2025-08-06 03:08:59 +0000

    更新 pom.xml 文件，修正 XML 格式并添加 JNA 依赖

---
+++ src/main/java/com/belonk/jna/PrintfDemo.java
@@ -0,0 +1,24 @@
+package com.belonk.jna;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+public class PrintfDemo {
+    // This is the standard, stable way of mapping, which supports extensive
+    // customization and mapping of Java to native types.
+
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.load((Platform.isWindows() ? "msvcrt" : "c"),
+                CLibrary.class);
+
+        void printf(String format, Object... args);
+    }
+
+    public static void main(String[] args) {
+        CLibrary.INSTANCE.printf("Hello, World\n");
+        for (int i = 0; i < args.length; i++) {
+            CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
+        }
+    }
+}
